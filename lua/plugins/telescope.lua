c2059cc6dfb995502428cd600a7732b85d41bbfb
mah <zanzinzzo@gmail.com>
2023-11-22 14:28:55 -0300

    fix treesitter and telescope keymapping is direct now

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = function()
-         return {
-            {'<leader>pf', '<cmd>Telescope find_files<cr>', desc = "find files"},
-            {'<leader>ps', '<cmd>Telescope grep_string<cr>', desc = "grep text from file"},
-            {'<C-p>', '<cmd>Telescope git_files<cr>', desc = "find git files"},
-        }
-    end
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "find files" },
+		{
+			"<leader>ps",
+			function()
+				require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+			end,
+			desc = "grep text from file",
+		},
+		{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "find git files" },
+	},
 }
