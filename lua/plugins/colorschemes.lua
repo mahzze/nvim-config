0f4cfa033291b975015e74abf526fecccbecd69e
mah <zanzinzzo@gmail.com>
2023-11-21 22:56:22 -0300

    add colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,14 @@
+return { 
+    -- the current theme is nordic.
+    {
+        'AlexvZyl/nordic.nvim',
+        name = 'nordic',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require 'nordic'.load()
+        end
+    },
+    {'catppuccin/nvim', name = 'catppuccin', priority = 1000},
+    {'nordtheme/vim', name = 'nord', priority = 1000},   
+}
