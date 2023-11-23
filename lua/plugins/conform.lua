1aa251495a5a584c49707c746460a0e54613041b
mah <zanzinzzo@gmail.com>
2023-11-22 21:23:00 -0300

    add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+    'stevearc/conform.nvim',
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                html = { "prettier" },
+                lua = { "stylua" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 250,
+            }
+        })
+        vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 250,
+            })
+        end, { desc = "Format" })
+    end
+}
