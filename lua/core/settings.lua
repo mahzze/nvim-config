09fba4fb958f2a630d0cd08bb5d6d826e5ad229e
mahzze <mahzze.dev@gmail.com>
2024-12-31 11:24:46 -0300

    chore: adds unnamedplus clipboard and turns off smart indent (may be changed to your preference)

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,4 +1,5 @@
 vim.opt.guicursor = "a:block"
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -8,7 +9,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+vim.opt.smartindent = false
 
 vim.opt.wrap = true
 
